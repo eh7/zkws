5447db5c35546198f694a0fcf56c6faf5e02c7a3
Gavin Francis <gav@eh7.co.uk>
2025-05-06 12:22:32 +0100

    update config data to get retieved from .env file, so no data is shown in git repo

--- apps/ws-relay-service/src/index.js
+++ apps/ws-relay-service/src/index.js
@@ -2,16 +2,22 @@ import Pop3Command from 'node-pop3'
 import 'dotenv/config'
 
 const pop3 = new Pop3Command({
-  user: 'bote@app.zkws.org',
+  user: process.env.BOT_USER,
   password: process.env.BOT_PASSWD,
-  host: 'mail0.zkws.org'
+  host: process.env.BOT_HOST,
 })
-await pop3.connect(),
+//await pop3.connect(),
+
+const list = await pop3.UIDL();
 
 console.log(
   //pop3
-  await pop3.command('STAT')
+  //await pop3.command('STAT')
+  list
 )
+
+pop3.command('QUIT')
+
 //const str = await pop3.command('LIST');
 
 //console.log(str)
