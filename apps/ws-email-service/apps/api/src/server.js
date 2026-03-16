879df32862005e5462d34993474872557d254db2
Gavin Francis <gav@eh7.co.uk>
2026-03-16 14:42:36 +0000

    ws-email-servcie :: add host to api server.js so this can be specified in the future

--- apps/ws-email-service/apps/api/src/server.js
+++ apps/ws-email-service/apps/api/src/server.js
@@ -11,7 +11,8 @@ app.use('/api/auth', authRoutes);
 app.use('/api/mail', mailRoutes);
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://${HOST}:${PORT}`);
 });
 
