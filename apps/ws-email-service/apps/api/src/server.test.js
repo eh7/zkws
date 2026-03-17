411b16a47bff80ce66e8d283d7272e8c32e14c8f
Gavin Francis <gav@eh7.co.uk>
2026-03-17 09:11:09 +0000

    ws-email-service/apps/api :: added some more testing of rest api WIP

--- apps/ws-email-service/apps/api/src/server.test.js
+++ apps/ws-email-service/apps/api/src/server.test.js
@@ -15,16 +15,42 @@ describe('Test server endpoints', () => {
   });
 
   it('returns a list of emails in maildir inbox', async () => {
-/*
-curl -X POST http://localhost:3000/api/auth/login \
-  -H "Content-Type: application/json" \
-  -d '{"email": "user@example.com", "password": "password123"}'
-*/
     const response = await request(server)
       .post('/api/auth/login')
       .set('Content-Type', 'application/json')
       .send({"email": "test@mail.com", "password": "password123"})
-    console.log(response.body)
+    expect(response.body.message).toBe('Invalid credentials');
+
+    const responseRegister = await request(server)
+      .post('/api/auth/register')
+      .set('Content-Type', 'application/json')
+      .send({"email": "user@example.com", "password": "password123"})
+    expect(responseRegister.body).toHaveProperty('token');
+
+    const tokenRegister = responseRegister.body.token;
+
+    const responseRegister1 = await request(server)
+      .post('/api/auth/register')
+      .set('Content-Type', 'application/json')
+      .send({"email": "user@example.com", "password": "password123"})
+    expect(responseRegister1.body.message).toBe('User already exists');
+
+    const responseListEmail = await request(server)
+      .get('/api/mail/list')
+      .set("x-auth-token", tokenRegister)
+    //console.log(Object.keys(responseListEmail.body).length)
+    //console.log(responseListEmail.body.length)
+    expect(responseListEmail.body.length).toBe(2);
+
+    const responseGetEmail1 = await request(server)
+      .get('/api/mail/1')
+      .set('Content-Type', 'application/json')
+      .set("x-auth-token", tokenRegister)
+    expect(responseGetEmail1.body).toHaveProperty('id');
+    expect(responseGetEmail1.body).toHaveProperty('from');
+    expect(responseGetEmail1.body).toHaveProperty('subject');
+    expect(responseGetEmail1.body).toHaveProperty('body');
+
 /*
     const response = await request(server)
       .get('/api/mail/list')
